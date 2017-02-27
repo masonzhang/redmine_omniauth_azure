9d0278a4e381391e886e2d3bbf58fb3e659481d2
7IN0SAN9 <me@7in0.me>
2017-02-27 20:03:19 +0800

    Update plugin metadata

--- init.rb
+++ init.rb
@@ -3,16 +3,16 @@ require_dependency 'redmine_omniauth_azure/hooks'
 
 Redmine::Plugin.register :redmine_omniauth_azure do
   name 'Redmine Omniauth Azure plugin'
-  author 'Gucin Tsui'
-  description 'This is a plugin for Redmine registration through Azure AD'
+  author 'JoyMoe IT'
+  description 'This is a plugin for Redmine authentication through Azure AD'
   version '0.0.1'
-  url 'https://github.com/ares/redmine_omniauth_azure'
-  author_url 'https://github.com/'
+  url 'https://git.shido.cn/IT/redmine_omniauth_azure'
+  author_url 'https://git.shido.cn/IT'
 
   settings :default => {
-    :client_id => "",
-    :client_secret => "",
-    :github_oauth_autentication => false,
-    :allowed_domains => ""
+    :client_id => '',
+    :client_secret => '',
+    :azure_oauth_authentication => false,
+    :allowed_domains => ''
   }, :partial => 'settings/azure_settings'
 end
