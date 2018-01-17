c80f2b3582642a569bad612c17730038f79482ec
hao.zhang <hao.zhang@avenue51.com>
2018-01-17 17:33:39 +0800

    代码备份：处理子项目非常多的情况，在项目浏览页去掉子项目列表

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,71 @@
+# encoding: utf-8
+#
+# Redmine - project management software
+# Copyright (C) 2006-2017  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require 'forwardable'
+require 'cgi'
+
+module ApplicationHelper
+  include Redmine::WikiFormatting::Macros::Definitions
+  include Redmine::I18n
+  include GravatarHelper::PublicMethods
+  include Redmine::Pagination::Helper
+  include Redmine::SudoMode::Helper
+  include Redmine::Themes::Helper
+  include Redmine::Hook::Helper
+  include Redmine::Helpers::URL
+
+  extend Forwardable
+  def_delegators :wiki_helper, :wikitoolbar_for, :heads_for_wiki_formatter
+
+
+  # Renders a tree of projects as a nested set of unordered lists
+  # The given collection may be a subset of the whole project tree
+  # (eg. some intermediate nodes are private and can not be seen)
+  def render_project_nested_lists(projects, &block)
+    s = ''
+    if projects.any?
+      ancestors = []
+      original_project = @project
+      projects.sort_by(&:lft).each do |project|
+        # set the project environment to please macros.
+        @project = project
+        next if !ancestors.empty? && project.is_descendant_of?(ancestors.last)
+
+        if (ancestors.empty? || project.is_descendant_of?(ancestors.last))
+          s << "<ul class='projects #{ ancestors.empty? ? 'root' : nil}'>\n"
+        else
+          ancestors.pop
+          s << "</li>"
+          while (ancestors.any? && !project.is_descendant_of?(ancestors.last))
+            ancestors.pop
+            s << "</ul></li>\n"
+          end
+        end
+        classes = (ancestors.empty? ? 'root' : 'child')
+        s << "<li class='#{classes}'><div class='#{classes}'>"
+        s << h(block_given? ? capture(project, &block) : project.name)
+        s << "</div>\n"
+        ancestors << project
+      end
+      s << ("</li></ul>\n" * ancestors.size)
+      @project = original_project
+    end
+    s.html_safe
+  end
+end
